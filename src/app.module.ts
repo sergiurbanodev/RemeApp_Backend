a72a5d363261a00b1a434400c17a13c20cd7af94
Sergi Urbano Alabró <sergi.urbano18@gmail.com>
2026-05-26 12:27:16 +0200

    feat: unified prisma and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller.js';
+import { AppConfigModule } from './config/config.module.js';
+import { PrismaModule } from './database/prisma.module.js';
+import { AuthModule } from './modules/auth/auth.module.js';
+import { AppService } from './app.service.js';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-  ],
+  imports: [AppConfigModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
