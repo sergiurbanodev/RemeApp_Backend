2d8ef0d03900f68c3e3e81593ca917477e037024
Sergi Urbano Alabró <sergi.urbano18@gmail.com>
2026-05-26 11:43:44 +0200

    feat: created prisma service and module

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
+import { PrismaPg } from '@prisma/adapter-pg';
 @Injectable()
 export class PrismaService
   extends PrismaClient
