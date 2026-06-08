6b39c3f7e3ed87bb98c4972607736b9a6460c86b
Sergi Urbano Alabró <sergi.urbano18@gmail.com>
2026-06-08 14:24:18 +0200

    feat: optional auth if endpoint uses user if logged but works unlogged

---
+++ src/common/guards/optional-jwt-auth/optional-jwt-auth.guard.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    return super.canActivate(context);
+  }
+
+  handleRequest<TUser = any>(err: any, user: any): TUser {
+    return user ?? null;
+  }
+}
